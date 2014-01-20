41b3373bbbb10591b1590c634c9b740545f6354f
Manuel Durán Aguete <manuel@aguete.org>
2014-01-20 16:57:34 +0100

    Version 3.2.2 cloned from  git@github.com:olgeni/amqp_client.git

--- src/amqp_direct_connection.erl
+++ src/amqp_direct_connection.erl
@@ -10,8 +10,8 @@
 %%
 %% The Original Code is RabbitMQ.
 %%
-%% The Initial Developer of the Original Code is VMware, Inc.
-%% Copyright (c) 2007-2012 VMware, Inc.  All rights reserved.
+%% The Initial Developer of the Original Code is GoPivotal, Inc.
+%% Copyright (c) 2007-2013 GoPivotal, Inc.  All rights reserved.
 %%
 
 %% @private
@@ -23,8 +23,10 @@
 
 -export([server_close/3]).
 
--export([init/1, terminate/2, connect/4, do/2, open_channel_args/1, i/2,
+-export([init/0, terminate/2, connect/4, do/2, open_channel_args/1, i/2,
          info_keys/0, handle_message/2, closing/3, channels_terminated/1]).
+
+-export([socket_adapter_info/2]).
 
 -record(state, {node,
                 user,
@@ -52,7 +54,7 @@ server_close(ConnectionPid, Code, Text) ->
                                 method_id  = 0},
     amqp_gen_connection:server_close(ConnectionPid, Close).
 
-init([]) ->
+init() ->
     {ok, #state{}}.
 
 open_channel_args(#state{node = Node,
@@ -118,7 +120,7 @@ connect(Params = #amqp_params_direct{username     = Username,
                                      node         = Node,
                                      adapter_info = Info,
                                      virtual_host = VHost},
-        SIF, _ChMgr, State) ->
+        SIF, _TypeSup, State) ->
     State1 = State#state{node         = Node,
                          vhost        = VHost,
                          params       = Params,
@@ -153,3 +155,52 @@ ensure_adapter_info(A = #amqp_adapter_info{name = unknown}) ->
     ensure_adapter_info(A#amqp_adapter_info{name = Name});
 
 ensure_adapter_info(Info) -> Info.
+
+socket_adapter_info(Sock, Protocol) ->
+    {PeerHost, PeerPort, Host, Port} =
+        case rabbit_net:socket_ends(Sock, inbound) of
+            {ok, Res} -> Res;
+            _          -> {unknown, unknown, unknown, unknown}
+        end,
+    Name = case rabbit_net:connection_string(Sock, inbound) of
+               {ok, Res1} -> Res1;
+               _Error     -> "(unknown)"
+           end,
+    #amqp_adapter_info{protocol        = Protocol,
+                       name            = list_to_binary(Name),
+                       host            = Host,
+                       port            = Port,
+                       peer_host       = PeerHost,
+                       peer_port       = PeerPort,
+                       additional_info = maybe_ssl_info(Sock)}.
+
+maybe_ssl_info(Sock) ->
+    case rabbit_net:is_ssl(Sock) of
+        true  -> [{ssl, true}] ++ ssl_info(Sock) ++ ssl_cert_info(Sock);
+        false -> [{ssl, false}]
+    end.
+
+ssl_info(Sock) ->
+    {Protocol, KeyExchange, Cipher, Hash} =
+        case rabbit_net:ssl_info(Sock) of
+            {ok, {P, {K, C, H}}}    -> {P, K, C, H};
+            {ok, {P, {K, C, H, _}}} -> {P, K, C, H};
+            _                       -> {unknown, unknown, unknown, unknown}
+        end,
+    [{ssl_protocol,     Protocol},
+     {ssl_key_exchange, KeyExchange},
+     {ssl_cipher,       Cipher},
+     {ssl_hash,         Hash}].
+
+ssl_cert_info(Sock) ->
+    case rabbit_net:peercert(Sock) of
+        {ok, Cert} ->
+            [{peer_cert_issuer,   list_to_binary(
+                                    rabbit_ssl:peer_cert_issuer(Cert))},
+             {peer_cert_subject,  list_to_binary(
+                                    rabbit_ssl:peer_cert_subject(Cert))},
+             {peer_cert_validity, list_to_binary(
+                                    rabbit_ssl:peer_cert_validity(Cert))}];
+        _ ->
+            []
+    end.
